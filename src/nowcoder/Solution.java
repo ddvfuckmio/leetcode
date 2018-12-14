5fc4b9cb3cd8e8933c6c027743b661dc83b20848
“drityddv” <1016653792@qq.com>
2018-12-14 17:24:20 +0800

    case

--- src/nowcoder/Solution.java
+++ src/nowcoder/Solution.java
@@ -1,36 +1,56 @@
 package nowcoder;
 
 import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class Solution {
+	private boolean sign = true;
+	private PriorityQueue<Integer> minQueue = new PriorityQueue<>(new MaxComparator());
+	private PriorityQueue<Integer> maxQueue = new PriorityQueue<>(new MinComparator());
 
-	public ArrayList<Integer> maxInWindows(int[] num, int size) {
-		ArrayList<Integer> list = new ArrayList<>();
-
-		if (size <= 0) return list;
-
-		for (int i = 0; i < num.length - size + 1; i++) {
-			list.add(help(num, i, size));
+	public void Insert(Integer num) {
+		if (sign) {
+			maxQueue.offer(num);
+			minQueue.offer(maxQueue.poll());
+		} else {
+			minQueue.offer(num);
+			maxQueue.offer(minQueue.poll());
 		}
-
-		return list;
-
+		sign = !sign;
 	}
 
-	private Integer help(int[] num, int index, int size) {
-		int max = num[index];
-
-		for (int i = index + 1; i < index + size - 1; i++) {
-			max = Math.max(max, num[i]);
+	public Double GetMedian() {
+		if(!sign){
+			return (double) minQueue.peek();
 		}
-
-		return max;
+		return (double) (minQueue.peek()+maxQueue.peek())/2;
 	}
+
 
 	public static void main(String[] args) throws Exception {
 		Solution solution = new Solution();
-		
+		PriorityQueue<Integer> heap = new PriorityQueue<>();
+		heap.offer(1);
+		heap.offer(10);
+		heap.offer(-1);
+
+		System.out.println(heap);
+
+	}
+
+	class MinComparator implements Comparator<Integer> {
+		@Override
+		public int compare(Integer o1, Integer o2) {
+			return o1 - o2;
+		}
+	}
+
+	class MaxComparator implements Comparator<Integer> {
+		@Override
+		public int compare(Integer o1, Integer o2) {
+			return o2 - o1;
+		}
 	}
 }
