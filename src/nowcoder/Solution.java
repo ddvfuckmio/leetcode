03704ada6ed766699e1fc815897dc0bc4a47ae5e
drityddv <1016653792@qq.com>
2019-01-05 00:34:51 +0800

    hello

--- src/nowcoder/Solution.java
+++ src/nowcoder/Solution.java
@@ -5,14 +5,21 @@ import codes.designs.TreeNode;
 import java.util.*;
 
 public class Solution {
-	ArrayList<ArrayList<Integer>> Print(TreeNode pRoot) {
-		ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+	public ArrayList<ArrayList<Integer> > Print(TreeNode pRoot) {
+		return null;
+	}
 
-		if (pRoot == null) return result;
 
-		Queue<TreeNode> queue1 = new LinkedList<>();
-		Queue<TreeNode> queue2 = new LinkedList<>();
-
-		return result;
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		TreeNode root = new TreeNode(1);
+		root.left = new TreeNode(2);
+		root.right = new TreeNode(3);
+		root.left.left = new TreeNode(4);
+		root.left.right = new TreeNode(6);
+		root.right.left = new TreeNode(7);
+		root.right.right = new TreeNode(8);
+		ArrayList<ArrayList<Integer>> list = solution.Print(root);
+		System.out.println(list);
 	}
 }
