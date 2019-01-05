1715dd928a97c562b9d5150da9e3235b6b9acad8
drityddv <1016653792@qq.com>
2019-01-05 18:26:02 +0800

    case

--- src/nowcoder/Solution.java
+++ src/nowcoder/Solution.java
@@ -2,11 +2,51 @@ package nowcoder;
 
 import codes.designs.TreeNode;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Solution {
-	public ArrayList<ArrayList<Integer> > Print(TreeNode pRoot) {
-		return null;
+	public ArrayList<ArrayList<Integer>> Print(TreeNode pRoot) {
+		ArrayList<ArrayList<Integer>> rs = new ArrayList<>();
+		if (pRoot == null) return rs;
+
+		int sign = 1;
+
+		LinkedList<TreeNode> queue = new LinkedList<>();
+		queue.offer(pRoot);
+
+		while (!queue.isEmpty()) {
+			ArrayList<Integer> list = new ArrayList<>();
+			int start, end;
+			if ((sign & 1) == 1) {
+				start = 0;
+				end = queue.size();
+				for (; start < end; start++) {
+					TreeNode node = queue.pollFirst();
+					list.add(node.val);
+					if (node.left != null)
+						queue.offerLast(node.left);
+					if (node.right != null)
+						queue.offerLast(node.right);
+				}
+				sign = sign ^ 1;
+			} else {
+				start = queue.size();
+				end = 0;
+				for (; start > end; start--) {
+					TreeNode node = queue.pollLast();
+					list.add(node.val);
+					if (node.right != null) queue.offerFirst(node.right);
+					if (node.left != null) queue.offerFirst(node.left);
+
+				}
+				sign = sign ^ 1;
+
+			}
+			rs.add(list);
+		}
+
+		return rs;
 	}
 
 
@@ -21,5 +61,6 @@ public class Solution {
 		root.right.right = new TreeNode(8);
 		ArrayList<ArrayList<Integer>> list = solution.Print(root);
 		System.out.println(list);
+
 	}
 }
